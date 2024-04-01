287d01dd87916ee8eea18870b2732e4a4fcd3fdb
Fi <maxwell.mclaughlin@ontariotechu.net>
2024-04-01 15:32:52 -0400

    init

---
+++ src/main/java/com/otu/HotelMain.java
@@ -0,0 +1,13 @@
+package com.otu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMain.class, args);
+	}
+
+}
---
+++ src/test/java/com/otu/SpringDataJpaotuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaotuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
