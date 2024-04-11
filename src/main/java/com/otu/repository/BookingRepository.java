d15ed705e786d58d583f23f9ce9fb9b694fde53a
Lexa <90271043+PreeminentLexa@users.noreply.github.com>
2024-04-10 23:48:20 -0400

    findByCustomerIDAndRoomID > findByCustomerAndRoom
    Fixed an issue where bookings were searched for using customer ID's when they shouldn't've

--- src/main/java/com/otu/repository/BookingRepository.java
+++ src/main/java/com/otu/repository/BookingRepository.java
@@ -7,12 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.otu.model.Booking;
+import com.otu.model.Customer;
+import com.otu.model.Room;
 
 public interface BookingRepository extends JpaRepository<Booking, Long>{
 	
 	//boolean existsByIdAndName(Long id, String name);
 	boolean existsByRoomId(Long roomId);
 
-	@Query("select b from Booking b where b.customerId = :customerId and b.roomId = :roomId")
-	List<Booking> findByCustomerIdAndRoomId(@Param ("customerId") Long customerId, @Param("roomId") Long roomId);
+	@Query("select b from Booking b where b.customer = :customer and b.room = :room")
+	List<Booking> findByCustomerAndRoom(@Param ("customer") Customer customer, @Param("room") Room room);
 }
