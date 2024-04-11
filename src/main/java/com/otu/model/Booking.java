f0b22b875829873a866e110f121aef3beff5d86c
Lexa <90271043+PreeminentLexa@users.noreply.github.com>
2024-04-10 23:34:59 -0400

    Fixed relations
    Hopefully I'm gonna actually upload the right files this time!

---
+++ src/main/java/com/otu/model/Booking.java
@@ -0,0 +1,119 @@
+package com.otu.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+
+@Entity
+public class Booking {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@ManyToOne
+	@JoinTable(name = "customer_bookings", joinColumns = { @JoinColumn(name = "booking_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "customer_id") })
+	private Customer customer;
+	@ManyToOne
+	@JoinTable(name = "room_bookings", joinColumns = { @JoinColumn(name = "booking_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "room_id") })
+	private Room room;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	
+	
+	
+	public Booking() {
+		super();
+	}
+
+	public Booking(long id, Customer customer, Room room, LocalDate startDate, LocalDate endDate) {
+		super();
+		this.id = id;
+		this.customer = customer;
+		this.room = room;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	
+	public boolean dateRangeIsValid() {
+		if(this.endDate == null) {
+			return false;
+		}
+		if(this.endDate == null) {
+			return false;
+		}
+		if(this.endDate.isBefore(this.startDate)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	// This is used when we have a list of bookings for a room, and we want to see what days are available
+	public boolean getDatesOverlap(LocalDate queryStartDate, LocalDate queryEndDate) {
+		if(this.startDate.isAfter(queryEndDate)) {
+			return true; // the whole query is before this's start date
+		}
+		if(this.endDate.isBefore(queryStartDate)) {
+			return true; // the whole query is after this's end date
+		}
+		return false; // there is some other overlap going on
+	}
+	
+	public boolean getDatesOverlap(Booking queryBooking) {
+		return this.getDatesOverlap(queryBooking.getStartDate(), queryBooking.getEndDate());
+	}
+
+	@Override
+	public String toString() {
+		return "Booking [id=" + id + ", customerId=" + customer.getId() + ", roomId=" + room.getId() + ", startDate=" + startDate
+				+ ", endDate=" + endDate + "]";
+	}
+	
+}
---
+++ src/main/java/com/otu/model/Room.java
@@ -0,0 +1,71 @@
+package com.otu.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Room {
+	@Id
+	@GeneratedValue
+	private long id;
+	private int roomNumber;
+	private String type;
+	private double price;
+	
+	@OneToMany(mappedBy = "room")
+	private List<Booking> bookings;
+
+	public Room() {
+		super();
+	}
+
+	public Room(long id, int roomNumber, String type, double price) {
+		super();
+		this.id = id;
+		this.roomNumber = roomNumber;
+		this.type = type;
+		this.type = type;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", roomNumber=" + roomNumber + ", type=" + type + ", price=" + price + "]";
+	}
+}
