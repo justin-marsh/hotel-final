49a01bcc72446deef0e85b6bf7eaf40c708887e5
Lexa <90271043+PreeminentLexa@users.noreply.github.com>
2024-04-11 19:38:54 -0400

    Add files via upload

--- src/main/java/com/otu/service/ProvidedServiceService.java
+++ src/main/java/com/otu/service/ProvidedServiceService.java
@@ -1,5 +1,6 @@
 package com.otu.service;
 
+import java.time.Month;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.otu.model.Customer;
-
+import com.otu.model.ProvidedService;
 import com.otu.repository.ServiceRepository;
 
 
@@ -22,7 +23,7 @@ public class ProvidedServiceService {
 	}
 
 	
-	public boolean addService(com.otu.model.ProvidedService providedService) {
+	public boolean addService(ProvidedService providedService) {
 		if(!repo.existsById(providedService.getId())) {
 			System.out.println("creating Service in db");
 			repo.save(providedService);
@@ -32,12 +33,21 @@ public class ProvidedServiceService {
 		return false;
 	}
 	
-	public List<com.otu.model.ProvidedService> getProvidedServices() {
-        //return Collections.<com.otu.model.ProvidedService>emptyList();
-		return repo.findAll();
+	public List<ProvidedService> getProvidedServices() {
+		com.otu.model.Room tempRoom = new com.otu.model.Room(1000l, 303, "suite", 50.00);
+		com.otu.model.Customer tempCustomer = new com.otu.model.Customer(1000l, "Lexa Valentine", "435-4444 pizza nova!", "justin.trudeau@canada.gov");
+		java.time.LocalDate tempStartDate = java.time.LocalDate.now(); 
+		java.time.LocalDate tempEndDate = java.time.LocalDate.of(2025, java.time.Month.JANUARY, 1);
+		com.otu.model.Booking tempBooking = new com.otu.model.Booking(1000l, tempCustomer, tempRoom, tempStartDate, tempEndDate);
+		return java.util.Arrays.asList(
+			new ProvidedService(1, "Mini bar", "1 bottle of Jack Daniels", 9999.99, tempBooking),
+			new ProvidedService(2, "Room service", "Cleaning", 10.00, tempBooking),
+			new ProvidedService(3, "Pay Per View", "NBA, NHL, NFL", 33.00, tempBooking)
+		);
+//		return repo.findAll();
     }
 	
-	public boolean validateService(com.otu.model.ProvidedService service) {
+	public boolean validateService(ProvidedService service) {
 		return repo.existsById(service.getId());
 	}
 
